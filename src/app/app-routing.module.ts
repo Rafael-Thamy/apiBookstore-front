4e5cfb614961153f072f12cce2b5b963b2ab4376
Rafael-Thamires <mnmrafa@gmail.com>
2021-10-05 18:45:12 -0300

    CRUD livro finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { RouterModule, Routes } from "@angular/router";
 import { CategoriaCreateComponent } from "./components/views/categoria/categoria-create/categoria-create.component";
 import { CategoriaDeleteComponent } from "./components/views/categoria/categoria-delete/categoria-delete.component";
 import { CategoriaUpdateComponent } from "./components/views/categoria/categoria-update/categoria-update.component";
+import { LivroReadAllComponent } from "./components/views/livro/livro-read-all/livro-read-all.component";
+import { LivroCreateComponent } from "./components/views/livro/livro-create/livro-create.component";
+import { LivroUpdateComponent } from "./components/views/livro/livro-update/livro-update.component";
+import { LivroDeleteComponent } from "./components/views/livro/livro-delete/livro-delete.component";
+import { LivroReadComponent } from "./components/views/livro/livro-read/livro-read.component";
 
 const routes: Routes = [
   {
@@ -26,7 +31,27 @@ const routes: Routes = [
   {
     path: "categorias/update/:id",
     component: CategoriaUpdateComponent,
-  }
+  },
+  {
+    path: "categorias/:id_cat/livros",
+    component: LivroReadAllComponent,
+  },
+  {
+    path: "categorias/:id_cat/livros/create",
+    component: LivroCreateComponent,
+  },
+  {
+    path: "categorias/:id_cat/livros/:id/update",
+    component: LivroUpdateComponent,
+  },
+  {
+    path: "categorias/:id_cat/livros/:id/delete",
+    component: LivroDeleteComponent,
+  },
+  {
+    path: "categorias/:id_cat/livros/:id/read",
+    component: LivroReadComponent,
+  },
 ];
 
 @NgModule({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: "https://api-livraria-back.herokuapp.com/",
 };
